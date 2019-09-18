e1e4593ba945d8bc030bb774483c20fa44bc649c
sbx_user1051 <sbx_user1051@169.254.191.189>
2019-09-18 23:10:23 +0000

    Wed Sep 18 23:10:23 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,6 +9,12 @@ special_offers = {'3A':130, '2B':45}
 def checkout(skus):
     total = 0
     for item in skus:
+        same_item_purchased = str(skus.count(item))+'item'
+        if same_item_purchased in special_offers:
+            total += special_offers[same_item_purchased]
+            skus.replace(item, '')
         if item in items_prices:
             total += items_prices[item]
-
+        else:
+            return -1
+    return total
