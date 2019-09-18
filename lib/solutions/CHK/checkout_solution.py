0e01cf79333fa9c0377759fd8782deef6b1cad99
sbx_user1051 <sbx_user1051@169.254.191.189>
2019-09-18 23:07:23 +0000

    Wed Sep 18 23:07:23 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,14 @@
 
+items_prices={'A':50,'B':30,'C':20,'D':15}
+special_offers = {'3A':130, '2B':45}
+
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    total = 0
+    for item in skus:
+        if item in items_prices:
+            total += items_prices[item]
+
